ff4e7fdd202614bf6f05138d1750b533fe8af836
Oluwaseye <seyz4all@gmail.com>
2019-03-29 17:49:26 -0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,120 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Keypad :keypad="keypad" :dialed="dialed" @dialNumber="combinations($event)"/>
+    <Result :resultsFound="resultsFound" :dialedletters="dialedletters" :viewData="viewData" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Keypad from './components/Keypad.vue'
+import Result from './components/Result.vue'
+import axios from 'axios'
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Keypad,
+    Result,
+    axios
+  },
+     data: function(){
+      return{
+            dictionary: [],
+            dialed: '',
+            keypad: false,
+            dialedletters: '',
+            viewData: [],
+            resultsFound: ''
+      }
+  },
+   mounted: function() {
+ 
+    axios.get('../data/names.json').then((response) => {
+         this.db = response.data
+    })
+    },
+  methods:{
+
+    getAllPermutations: function(digits) {
+      if (digits.length === 0) return [];
+    
+      var map = {
+        "2": ["a", "b", "c"],
+        "3": ["d", "e", "f"],
+        "4": ["g", "h", "i"],
+        "5": ["j", "k", "l"],
+        "6": ["m", "n", "o"],
+        "7": ["p", "q", "r", "s"],
+        "8": ["t", "u", "v"],
+        "9": ["w", "x", "y", "z"]
+      };
+        var combinations = [''];
+        
+        // for as many times as there are digits (eg. '3721' => 4 times)
+        for (var i = 0; i < digits.length; i++) {
+            var digit = digits[i];
+            var letters = map[digit];
+            var tempArray = [];
+            
+            // skip if invalid digit
+            if (letters === undefined) continue;
+            
+            // for as many times as there are letters (eg. 'abc' => 3 times)
+            for (var j = 0; j < letters.length; j++) {
+                var letterToAdd = letters[j];
+                // for as many times as there are existing combinations
+                for (var k = 0; k < combinations.length; k++) {
+                    var combination = combinations[k];
+                    tempArray.push(combination + letterToAdd);
+                }
+            }
+            combinations = tempArray;
+        }
+        return combinations;
+    },
+    search: function(s){
+      var arrival = s.toLowerCase();
+      var match = (!arrival.length ? '' :
+        this.db.filter(function (place) {
+          // look for the entry with a matching `code` value
+         return (place.word.name.toLowerCase() === arrival);
+         
+        }).map(function (place) {
+          // get titles of matches
+          //return [place.word.name, place.word.desc.substring(0, 100) + "..."];
+         return place.word.name;
+        }));
+        //console.log(match.length);
+        if(match !== undefined && match.length !== 0){
+          return match;
+        }
+        
+    },
+   
+    combinations: function (e) {
+      this.dialed += e.target.dataset.num;
+      this.dialedletters += e.target.dataset.letters;
+      // //if user dialed more than one number
+       var perm = this.getAllPermutations(this.dialed);
+  
+      if (this.dialed.length === 3 ) {
+        this.keypad = true;
+         
+        perm.forEach(string => {
+          if (this.search(string) && this.search(string).length) {
+            this.viewData.push(this.search(string));
+           
+          }
+        });
+        
+       this.resultsFound = this.viewData.length;
+        
+       }
+    }
   }
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+ @import './assets/css/style.css';
 </style>
---
+++ src/components/Keypad.vue
@@ -0,0 +1,48 @@
+<template>
+        <div id="phone">
+          <div id="wrapper">
+            <div id="dialbox" ref="dialbox" v-cloak>{{dialed}}</div>            
+            <div id="keypad">
+              <a class="key" :data-num="1"  @click="dial" v-bind:class="{'disabled': keypad}">1</a>
+              <a class="key" :data-num="2" :data-letters="'abc'" ref="btn" @click="dial" v-bind:class="{'disabled': keypad}">2<span>abc</span></a>
+              <a class="key" :data-num="3" :data-letters ="'def'"  ref="btn"  @click="dial" v-bind:class="{'disabled': keypad}">3<span>def</span></a>
+              <div class="clear"></div>
+              <a class="key" :data-num="4" :data-letters ="'ghi'"  ref="btn"  @click="dial" v-bind:class="{'disabled': keypad}">4<span>ghi</span></a>
+              <a class="key" :data-num="5" :data-letters ="'jkl'"  ref="btn"  @click="dial" v-bind:class="{'disabled': keypad}">5<span>jkl</span></a>
+              <a class="key" :data-num="6" :data-letters ="'mno'"  ref="btn"  @click="dial" v-bind:class="{'disabled': keypad}">6<span>mno</span></a>
+              <div class="clear"></div>
+              <a class="key" :data-num="7" :data-letters ="'pqrs'"  ref="btn"  @click="dial" v-bind:class="{'disabled': keypad}">7<span>pqrs</span></a>
+              <a class="key" :data-num="8" :data-letters ="'tuv'"  ref="btn"  @click="dial" v-bind:class="{'disabled': keypad}">8<span>tuv</span></a>
+              <a class="key" :data-num="9" :data-letters ="'wxyz'"  ref="btn"  @click="dial" v-bind:class="{'disabled': keypad}">9<span>wxyz</span></a>
+              <div class="clear"></div>
+              <div class="key special disabled" rel="*"  v-bind:class="{'disabled': keypad}">*</div>
+              <a class="key" :data-num="0" @click="dial" v-bind:class="{'disabled': keypad}">0<span>oper</span></a>
+              <div class="key special disabled" rel="#"  v-bind:class="{'disabled': keypad}"> #</div>
+              <div class="clear" ></div>
+              <div class="key nb disabled"></div>
+              <div class="key nb disabled"  rel="Make Call" ref="makecall"></div>
+              <div class="key nb disabled"></div>
+              <div class="clear"></div>
+            </div>
+          </div>
+        </div>
+</template>
+
+<script>
+export default { 
+  name: "Keypad",
+  props :{
+    keypad: Boolean,
+    dialed: String,
+    ele: Object,
+  },
+  methods:{
+    dial(){
+      this.$emit('dialNumber', window.event);
+    }
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
