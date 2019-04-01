0947b27928d07d16477233b09f1577a0f8450d6b
Oluwaseye <seyz4all@gmail.com>
2019-04-01 17:23:45 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/css/style.less'
+Vue.filter('pluralize', (word, amount) => amount > 1 ? `${word}s` : word)
 
-//export const serverBus = new Vue();
 Vue.config.productionTip = false
+Vue.config.silent = true
 
 new Vue({
   render: h => h(App),
